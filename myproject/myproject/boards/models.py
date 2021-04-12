5d528605dd53cac3b41f8c1d3f0d93335831ec56
daniel <oly.leyva62@gmail.com>
2021-04-12 13:00:02 -0700

    paso 3

--- myproject/myproject/boards/models.py
+++ myproject/myproject/boards/models.py
@@ -25,4 +25,4 @@ class Post(models.Model):
     updated_at = models.DateTimeField(null=True)
     created_by = models.ForeignKey(User, related_name='posts', on_delete=models.DO_NOTHING)
     updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.DO_NOTHING)
-    updated_by = models.ForeignKey(User, null=True, related_name='+')   
+    updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.DO_NOTHING)   
--- myproject/myproject/boards/views.py
+++ myproject/myproject/boards/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Board
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+
 
 def home(request):
     boards = Board.objects.all()
@@ -14,4 +16,13 @@ def home(request):
 
     return HttpResponse(response_html)
     
-   
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
+
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404
+    return render(request, 'topics.html', {'board': board})
